1369d4b2b50a794cd6a7192e63cb77ac017f44e1
Minseong Cho <minseoi.cc@gmail.com>
2024-12-15 18:12:22 +0900

    로그 수정

--- Source/Log/LogDefinition.h
+++ Source/Log/LogDefinition.h
@@ -3,7 +3,7 @@
 #pragma once
 
 #define LOG(level, format, ...) \
-std::printf("[%s] (%s/%d) " format "\n", level, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+std::printf("[%s] (%s/%d) " format "\n", level, __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
 
 #define LOG_D(format, ...) \
 LOG("DEBUG", format, ##__VA_ARGS__)
