d9e06293596cde39e34362b858cfa5a439cf5fac
Minseong Cho <minseoi.cc@gmail.com>
2024-12-15 18:06:00 +0900

    Update Vector.hpp

--- Source/Math/Vector.hpp
+++ Source/Math/Vector.hpp
@@ -2,14 +2,39 @@
 
 #pragma once
 
-struct Vector {
+struct Vector
+{
     float X;
     float Y;
     float Z;
+
     Vector(const float _x, const float _y, const float _z) :
         X(_x),
         Y(_y),
         Z(_z)
     {
     }
+
+    Vector(const Vector& other) = default;
+
+    Vector operator + (const Vector& other) const {
+        return {X + other.X, Y + other.Y, Z + other.Z};
+    }
+
+    Vector operator - (const Vector& other) const {
+        return {X - other.X, Y - other.Y, Z - other.Z};
+    }
+
+    Vector operator * (const float scalar) const {
+        return {X * scalar, Y * scalar, Z * scalar};
+    }
+
+    Vector& operator += (const Vector& other) {
+        X += other.X; Y += other.Y; Z += other.Z;
+        return *this;
+    }
+
+    static Vector ZeroVector;
 };
+
+inline Vector Vector::ZeroVector = {0, 0, 0};
